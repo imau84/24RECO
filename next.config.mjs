a657e73c3fa1aaf66f3500ceb642b35ba8a2516b
imau84 <marius.igas@gmail.com>
2026-06-01 21:30:04 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/Industrii%20-%20Imobiliare.html',
+        destination: '/imobiliare',
+        permanent: true,
+      },
+      {
+        source: '/Industrii - Imobiliare.html',
+        destination: '/imobiliare',
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig;
