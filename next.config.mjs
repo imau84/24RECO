6786470000d06ef9887ea12e236c3df24e91a3e0
imau84 <marius.igas@gmail.com>
2026-09-15 21:54:15 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/Industrii%20-%20Imobiliare.html',
-        destination: '/imobiliare',
-        permanent: true,
-      },
-      {
-        source: '/Industrii - Imobiliare.html',
-        destination: '/imobiliare',
-        permanent: true,
-      },
-    ];
-  },
+    async redirects() {
+          return [
+            { source: '/Industrii%20-%20Imobiliare.html', destination: '/imobiliare', permanent: true },
+            { source: '/Industrii - Imobiliare.html', destination: '/imobiliare', permanent: true },
+            { source: '/Industrii%20-%20Constructii.html', destination: '/industrii/constructii', permanent: true },
+            { source: '/Industrii - Constructii.html', destination: '/industrii/constructii', permanent: true },
+            { source: '/Industrii%20-%20Transport.html', destination: '/transport', permanent: true },
+            { source: '/Industrii - Transport.html', destination: '/transport', permanent: true },
+            { source: '/Rapoarte%20-%20Situatii%20Financiare.html', destination: '/rapoarte/date-financiare', permanent: true },
+            { source: '/Rapoarte - Situatii Financiare.html', destination: '/rapoarte/date-financiare', permanent: true },
+                ];
+    },
 };
 export default nextConfig;
